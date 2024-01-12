0f22630c83e35f98ef8561e9067d9de74b1425ad
Bideauquen <102724149+Bideauquen@users.noreply.github.com>
2024-01-12 10:42:15 +0100

    News

--- front/function.py
+++ front/function.py
@@ -40,10 +40,33 @@ def update_coordinates(api_key, restaurants):
 
 # Remplacez 'VOTRE_CLE_API' par votre clé API Google Maps
 api_key = 'AIzaSyBE6J0FRoS4EJ0AVv6bILp-qzU7q1zHsmo'
+def maj_coord():
+    # Charger le contenu du fichier JSON dans un dictionnaire
+    with open('../restaurants.json', 'r') as file:
+        data = json.load(file)
 
-# Charger le contenu du fichier JSON dans un dictionnaire
-with open('../restaurants.json', 'r') as file:
-    data = json.load(file)
+    # Mettre à jour les coordonnées
+    update_coordinates(api_key, data["restaurants"])
 
-# Mettre à jour les coordonnées
-update_coordinates(api_key, data["restaurants"])
+def maj_rating():
+    # Charger les données depuis les fichiers JSON
+    with open('static/companies.json', 'r') as companies_file:
+        companies_data = json.load(companies_file)
+
+    with open('static/restaurants.json', 'r') as restaurants_file:
+        restaurants_data = json.load(restaurants_file)
+
+    # Créer un dictionnaire pour mapper l'id de la company à ses ratings
+    company_ratings = {company['id']: company['ratings'] for company in companies_data['companies']}
+
+    # Parcourir les restaurants et ajouter le champ "rating" en fonction de l'id de la company
+    for restaurant in restaurants_data:
+        company_id = restaurant['company']
+        rating = company_ratings.get(company_id, "No rating available")
+        restaurant['rating'] = rating.split(', ')  # Séparer les ratings en une liste
+
+    # Enregistrer les modifications dans restaurants.json
+    with open('static/restaurants.json', 'w') as restaurants_file:
+        json.dump(restaurants_data, restaurants_file, indent=2)
+
+maj_rating()
