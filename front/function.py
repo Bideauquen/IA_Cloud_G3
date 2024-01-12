e448ae1eacc6e71a62a58ad4fcf0578ec441a033
Bideauquen <102724149+Bideauquen@users.noreply.github.com>
2024-01-12 10:02:51 +0100

    News

--- front/function.py
+++ front/function.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def get_lat_long(api_key, address):
     base_url = "https://maps.googleapis.com/maps/api/geocode/json"
@@ -18,15 +19,31 @@ def get_lat_long(api_key, address):
             longitude = location['lng']
             return latitude, longitude
         else:
-            print(f"Erreur: {data['status']}")
+            print(f"Erreur lors de la géocodification de {address}: {data['status']}")
             return None
     except Exception as e:
-        print(f"Une erreur s'est produite : {e}")
+        print(f"Une erreur s'est produite lors de la géocodification de {address}: {e}")
         return None
+
+def update_coordinates(api_key, restaurants):
+    for restaurant in restaurants:
+        address = restaurant['address']
+        coordinates = get_lat_long(api_key, address)
+        print(f"Coordonnées de {address}: {coordinates}")
+        # écrire par dessus les anciennes coordonnées dans le fichier
+        restaurant['latitude'] = coordinates[0]
+        restaurant['longitude']= coordinates[1]
+        restaurant['coordinates'] = coordinates
+    with open('../restaurants.json', 'w') as file:
+        dict = {"restaurants": restaurants}
+        json.dump(dict, file, ensure_ascii=False, indent=4)
 
 # Remplacez 'VOTRE_CLE_API' par votre clé API Google Maps
 api_key = 'AIzaSyBE6J0FRoS4EJ0AVv6bILp-qzU7q1zHsmo'
-adresse = '10B rue Pilar, 64000 Pau'
 
-coordinates = get_lat_long(api_key, adresse)
-print(coordinates)
+# Charger le contenu du fichier JSON dans un dictionnaire
+with open('../restaurants.json', 'r') as file:
+    data = json.load(file)
+
+# Mettre à jour les coordonnées
+update_coordinates(api_key, data["restaurants"])
